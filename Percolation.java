bd0e8cf67752781acdaaf94e897ad6c77a0ba60b
youngjae94 <youngjae1994@gmail.com>
2020-03-27 04:30:39 -0500

    added code in Percolation and summary of git commands in hello.md

--- Percolation.java
+++ Percolation.java
@@ -7,29 +7,49 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
     private boolean [][] grid;
     private WeightedQuickUnionUF qu;
+    private int lm; // length of matrix
 
     // creates n-by-n grid, with all sites initially blocked. 0 = closed, 1= open
     private Percolation(int n) {
         grid = new boolean [n][n];
         if (n <= 0) throw new IllegalArgumentException("The input n <= 0");
         qu = new WeightedQuickUnionUF(n*n + 1);
+        lm = n;
+        System.out.println(qu);
         /*for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 grid[i][j] = false;
             }
         }*/
-        System.out.println(Arrays.deepToString(grid));
+        //System.out.println(Arrays.deepToString(grid));
     }
 
     // opens the site (row, col) if it is not open already. 0 = closed, 1 = open
     public void open(int row, int col) {
-        if (!grid[row][col])
-            grid[row][col] = true;
+
+        //if (!grid[row][col])
+          //  grid[row][col] = true;
+        if (checkValidIndex(row, col)){
+            if(!isOpen(row, col)){
+                grid[row][col] = true;
+            }
+        }else{
+            throw new IndexOutOfBoundsException("error1");
+        }
+    }
+    // checks the spot whether the index is valid or not
+    private boolean checkValidIndex(int row, int col) {
+        if(row < 1 || row > lm || col < 1 || col > lm) return false;
+        return true;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return grid[col][row];
+        if (checkValidIndex(row, col)){
+            return grid[col][row];
+        }else{
+            throw new IndexOutOfBoundsException("error2");
+        }
     }
 
     // is the site (row, col) full?
